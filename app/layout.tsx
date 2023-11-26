d7718ff11379ee1fefa5541b200424fa99afbbe5
jonathanweiss0 <jon.weiss94@mgmail.com>
2023-11-26 17:36:20 -0500

    Getting Caught up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,31 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
-import { ModalProvider } from '@/providers/modal.provider'
-import { ToasterProvider } from '@/providers/toast-provider'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from "@/providers/modal.provider";
+import { ToasterProvider } from "@/providers/toast-provider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'JW Jewelers Admin Dashboard',
-  description: 'Admin Dashboard',
-}
+  title: "JW Jewelers Admin Dashboard",
+  description: "Admin Dashboard",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>
-        <ToasterProvider />
-        <ModalProvider />
-        {children}</body>
-    </html>
+      <html lang="en">
+        <body className={inter.className}>
+          <ToasterProvider />
+          <ModalProvider />
+          {children}
+        </body>
+      </html>
     </ClerkProvider>
-  )
+  );
 }
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -5,73 +5,94 @@ import { Modal } from "@/components/ui/modal";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
 
-const formSchema = z.object ({
-    name: z.string().min(1),
+const formSchema = z.object({
+  name: z.string().min(1),
 });
 
-
 export const StoreModal = () => {
-    const storeModal = useStoreModal();
+  const storeModal = useStoreModal();
 
-    const [loading, setLoading] = useState(false);
-    
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            name: "",
-        },
-    });
+  const [loading, setLoading] = useState(false);
 
-    const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        try {
-            setLoading(true);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
 
-            const response = await axios.post('/api/stores', values);
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true);
 
-            toast.success("Store created.");
+      const response = await axios.post("/api/stores", values);
 
-        } catch (error) {
-            toast.error("Something went wrong.");
-
-        } finally {
-            setLoading(false);
-        }
+      window.location.assign(`/${response.data.id}`);
+    } catch (error) {
+      toast.error("Something went wrong.");
+    } finally {
+      setLoading(false);
     }
-    
-    return (
-    <Modal title="Create store" description="Add a new store to manage products and categories" isOpen={storeModal.isOpen} onClose={storeModal.onClose}>
-        <div>
+  };
+
+  return (
+    <Modal
+      title="Create store"
+      description="Add a new store to manage products and categories"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      <div>
         <div className="py-2 pb-4 space-y-4">
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)}>
-                    <FormField
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Name</FormLabel>
-                            <FormControl>
-                                <Input disabled={loading} placeholder="JW-Jewelers" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                    />
-                    <div className="flex items-center justify-end w-full pt-6 space-x-2">
-                        <Button disabled={loading} variant="outline" onClick={storeModal.onClose}>Cancel</Button>
-                        <Button  disabled={loading} type="submit">Continue</Button> 
-                    </div>
-                </form>
-            </Form>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={loading}
+                        placeholder="JW-Jewelers"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex items-center justify-end w-full pt-6 space-x-2">
+                <Button
+                  disabled={loading}
+                  variant="outline"
+                  onClick={storeModal.onClose}
+                >
+                  Cancel
+                </Button>
+                <Button disabled={loading} type="submit">
+                  Continue
+                </Button>
+              </div>
+            </form>
+          </Form>
         </div>
-        </div>
+      </div>
     </Modal>
-    );
+  );
 };
